0b179f487af8f34cc1b6f54afcb9a85d2c703330
Teo-PENNECOT <97228161+Teo-PENNECOT@users.noreply.github.com>
2023-07-06 14:04:43 +0100

    Homepage finished

--- src/main/java/com/example/movie_ticket_booking/HomeController.java
+++ src/main/java/com/example/movie_ticket_booking/HomeController.java
@@ -1,4 +1,43 @@
 package com.example.movie_ticket_booking;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+
 public class HomeController {
+    @FXML
+    private ComboBox<?> combo_date;
+
+    @FXML
+    private ComboBox<?> combo_movie;
+
+    @FXML
+    private ComboBox<?> combo_seat;
+
+    @FXML
+    private ComboBox<?> combo_venue;
+
+    @FXML
+    private Button login_btn;
+
+    @FXML
+    private ImageView logo;
+
+    @FXML
+    private ImageView logo1;
+
+    @FXML
+    private ScrollPane scroll;
+
+    @FXML
+    private Button search_qb;
+
+    @FXML
+    private TextField searchbar;
+
+    @FXML
+    private Button signin_btn;
 }
