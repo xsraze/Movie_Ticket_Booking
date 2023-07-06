b43d20788ac9ea0a7ae203980ec18af6879d271c
Teo-PENNECOT <97228161+Teo-PENNECOT@users.noreply.github.com>
2023-07-06 13:04:17 +0100

    Creation of some pages

---
+++ src/main/java/com/example/movie_ticket_booking/LoginController.java
@@ -0,0 +1,16 @@
+package com.example.movie_ticket_booking;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class LoginController {
+    @FXML
+    private TextField Pw_txt;
+
+    @FXML
+    private Button Submit_login;
+
+    @FXML
+    private TextField User_txt;
+}
