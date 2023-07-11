ca4d7b3ce243863dc6fd4892fc5ac398fe9e44b1
xsraze <elliothoms4@gmail.com>
2023-07-11 13:26:44 +0200

    Update Movie beta

---
+++ src/main/java/com/example/movie_ticket_booking/MovieUpdateController.java
@@ -0,0 +1,38 @@
+package com.example.movie_ticket_booking;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
+public class MovieUpdateController {
+    @FXML
+    private TextField txtGenre;
+
+    @FXML
+    private TextField txtName;
+
+    @FXML
+    private TextField txtPoster;
+
+    @FXML
+    private TextField txtRealisator;
+
+    @FXML
+    private TextField txtResume;
+
+    @FXML
+    private TextField txtReview;
+
+    @FXML
+    private TextField txtYear;
+
+    @FXML
+    void DeleteMovie(ActionEvent event) {
+
+    }
+
+    @FXML
+    void UpdateMovie(ActionEvent event) {
+
+    }
+}
